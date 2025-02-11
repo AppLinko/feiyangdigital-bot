0aef661d41f4d789ea974d86abb07de4fc4966a5
叫我沈同学 <97521809+ClassmateShen@users.noreply.github.com>
2025-02-11 20:20:16 +0800

    Fix Bug

--- src/main/java/top/feiyangdigital/utils/groupCaptch/SetBot.java
+++ src/main/java/top/feiyangdigital/utils/groupCaptch/SetBot.java
@@ -14,6 +14,7 @@ import top.feiyangdigital.sqlService.GroupInfoService;
 import top.feiyangdigital.utils.CheckUser;
 import top.feiyangdigital.utils.SendContent;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import top.feiyangdigital.utils.TimerDelete;
 
 @Component
 public class SetBot {
